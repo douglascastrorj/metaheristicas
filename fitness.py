be1b5d93a6312decd0d955acd443106f938c6d5a
Douglas <douglascastrorj@gmail.com>
2020-12-13 16:28:47 -0300

    adicionando funcao fitness

---
+++ fitness.py
@@ -0,0 +1,61 @@
+from utils import filterNotBy, getPenalizacao
+
+def fitnessFunction( cirurgias, S, T, D, Xcstd, z):
+
+    cirurgiasNaoP1 = filterNotBy(cirurgias, 'p', 1)
+
+    fo = 0
+
+    for c in cirurgias:
+        cirurgia = cirurgias[c]
+        wc = cirurgia['w']
+        for s in range(0, S):
+            for t in range(0,T):
+                xcst1 = Xcstd[c][s][t][0]
+                fo += -(wc + 3)*xcst1
+
+                # print(fo)
+
+    for c in cirurgiasNaoP1:
+        cirurgia = cirurgiasNaoP1[c]
+        wc = cirurgia['w']
+        for s in range(0, S):
+            for t in range(0, T):
+                for d in range(0, D):
+                    xcstd = Xcstd[c][s][t][d]
+                    fo += -(wc + 2 + (d + 1))*xcstd # nosso d vai de 0 a 4
+        pc = cirurgia['p']
+        Epc = getPenalizacao(pc)
+        fo += pc * Epc * z[c]
+
+    return fo
+
+def fitnessFunction2( cirurgias, S, T, D, Xcstd, z):
+
+    cirurgiasNaoP1 = filterNotBy(cirurgias, 'p', 1)
+
+    fo = 0
+
+    for c in cirurgias:
+        cirurgia = cirurgias[c]
+        wc = cirurgia['w']
+        for s in range(0, S):
+            for t in range(0,T):
+                xcst1 = Xcstd[c][s][t][0]
+                fo += -(wc + 3)*xcst1
+
+        pc = cirurgia['p']
+        Epc = getPenalizacao(pc)
+        fo += pc * Epc * z[c]
+
+    for c in cirurgiasNaoP1:
+        cirurgia = cirurgiasNaoP1[c]
+        wc = cirurgia['w']
+        for s in range(0, S):
+            for t in range(0, T):
+                for d in range(0, D):
+                    xcstd = Xcstd[c][s][t][d]
+                    fo += -(wc + 2 + (d + 1))*xcstd # nosso d vai de 0 a 4
+        
+
+    return fo
--- teste.py
+++ teste.py
@@ -1,6 +1,6 @@
 from utils import readDataset, createMap, filterBy, getDistinctSpecialtyArr
 from guloso2 import gerarSolucaoInicial
-import json
+from fitness import fitnessFunction, fitnessFunction2
 
 S = 2
 T = 46
@@ -13,8 +13,11 @@ solucaoInicial, yesd, z = gerarSolucaoInicial(cirurgias, S, D, verbose=True)
 
 print(solucaoInicial)
 
-print('\n')
-print(z)
+fo = fitnessFunction(cirurgias, S, T, D, solucaoInicial, z)
+fo2 = fitnessFunction2(cirurgias, S, T, D, solucaoInicial, z)
+print(f'FO = {fo}')
+print(f'FO2 = {fo2}')
+
 
 # implementar metodo de busca local
 #   - verificar se solucao é viavel
